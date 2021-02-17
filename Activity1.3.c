8699795b6f89964d0ef047ee725f7b2f46a48a90
Majo088 <78566871+Majo088@users.noreply.github.com>
2021-02-17 12:23:40 -0600

    Add files via upload

---
+++ Activity1.3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+  
+ int main ()
+{
+ int fahr, celsius;
+ int lower, upper, step;
+
+ lower= 0;
+ upper= 300;
+ step= 20;
+
+ fahr= lower;
+ printf("Convesion table\n");
+ 
+ while(fahr<=upper){
+        celsius = 5 * (fahr-32) / 9;
+        printf("%d\t%d\n", fahr,celsius);
+        fahr= fahr + step;
+        }
+
+  return 0;
+}
+
