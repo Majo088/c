7824b3a3caa4687e0b2b263f65b80c149b60fa57
Majo088 <78566871+Majo088@users.noreply.github.com>
2021-03-03 21:21:09 -0600

    Add files via upload

---
+++ homework/Homework2.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<math.h>
+
+#define M 15
+char*prime(int n);
+char*binary(int j);
+
+int main(void)
+{
+   prime(M);
+}
+
+
+char*binary(int j)
+{
+    int i=0;
+
+    int a[6]={0};
+
+    while(j>0)
+    {
+a[i]= j % 2;
+j /= 2;
+++i;
+    }
+
+for(int x=5; x >= 0 ; -- x)
+    {
+printf("%1d",a[x]);
+    }
+printf("\n");
+}
+
+//start of the prime numbers//
+char*prime(k)
+{
+  int count, i, is, sr;
+  int n=1;
+  count = 1;
+
+  do{
+    sr = sqrt(n);
+    is = 0;
+    for(i=2; i<= sr; i++){
+      if(n % i == 0){
+        is = 1;
+        break;
+      }
+
+    }
+
+    if(is == 0){
+
+      binary(n);
+      count++;
+    }
+
+    n++;
+
+  } while(count <= M);
+
+  return 0;
+
+}
+
