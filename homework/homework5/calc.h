b9b4d9780e08ee754c66c951fb1199202a21921f
Majo088 <78566871+Majo088@users.noreply.github.com>
2021-04-22 08:53:20 -0500

    Add files via upload

---
+++ homework/homework5/calc.h
@@ -0,0 +1,15 @@
+
+/*calc.h file*/
+#define MINA_ENCONTRADA 1
+#define ESPACIO_YA_DESCUBIERTO 2
+#define NINGUNO 3
+
+#define COLUMNAS 10
+#define FILAS 10
+#define ESPACIO_SIN_DESCUBRIR '#'
+#define ESPACIO_DESCUBIERTO '*'
+#define MINA 'X'
+#define CANTIDAD_MINAS \
+  6  
+  
+
